e6431af58c518cc0cd089472894de6b8009c5f88
John Leach <john@brightbox.co.uk>
2018-04-12 17:44:45 +0100

    Fix webrick test_bad_username_with_control_characters test
    Test was incorrectly backported (the actual fix was backported correctly).

--- test/ruby/envutil.rb
+++ test/ruby/envutil.rb
@@ -213,6 +213,39 @@ module Test
       def assert_is_minus_zero(f)
         assert(1.0/f == -Float::INFINITY, "#{f} is not -0.0")
       end
+
+     # threads should respond to shift method.
+      # Array can be used.
+      def assert_join_threads(threads, message = nil)
+        errs = []
+        values = []
+        while th = threads.shift
+          begin
+            values << th.value
+          rescue Exception
+            errs << [th, $!]
+          end
+        end
+        if !errs.empty?
+          msg = "exceptions on #{errs.length} threads:\n" +
+            errs.map {|t, err|
+            "#{t.inspect}:\n" +
+            err.backtrace.map.with_index {|line, i|
+              if i == 0
+                "#{line}: #{err.message} (#{err.class})"
+              else
+                "\tfrom #{line}"
+              end
+            }.join("\n")
+          }.join("\n---\n")
+          if message
+            msg = "#{message}\n#{msg}"
+          end
+          raise MiniTest::Assertion, msg
+        end
+        values
+      end
+
     end
   end
 end
